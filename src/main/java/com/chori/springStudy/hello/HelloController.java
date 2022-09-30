161e581a663db9d4fea02d99b5f7a7ab532a9171
Brian <choryee@naver.com>
2022-09-30 13:01:59 +0900

    hello world

---
+++ src/main/java/com/chori/springStudy/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.chori.springStudy.hello;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/home")
+    public String home(){
+        System.out.println("test!!==================");
+                return "home";
+    }
+}
